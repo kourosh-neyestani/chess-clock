e69bc435808953aee2c9a472cbe1f0ea5695232c
Kourosh-Neyestani <kourosh.neyestani@gmail.com>
2020-04-01 19:50:06 +0430

    add structure and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import './App.css';
+import './app.scss';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <div className="chess-clock">
+                <div className="player-1">
+                    <div className="display-center">
+                        <span className="timer">00:00:00</span>
+                    </div>
+                </div>
+                <div className="player-2">
+                    <div className="display-center">
+                        <span className="timer">00:00:00</span>
+                    </div>
+                </div>
+                <div className="navigation display-center">
+                    <ul>
+                        <li>
+                            <span className="icon icon-gear"></span>
+                        </li>
+                        <li>
+                            {/* <span className="icon icon-pause"></span> */}
+                            <span className="icon icon-play"></span>
+                        </li>
+                        <li>
+                            <span className="icon icon-reload"></span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
